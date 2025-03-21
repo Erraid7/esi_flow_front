62efd196b2f5f5fc22621baceb0021312b0cb5a7
yasmiinie <ly_zaidi@esi.dz>
2025-03-21 18:03:09 +0100

    adding favicon

--- pages/_app.js
+++ pages/_app.js
@@ -1,14 +1,19 @@
+import Head from "next/head";
 import "@/styles/globals.css";
 import { LanguageProvider } from "./translations/contexts/languageContext";
 
-
-
-
 function MyApp({ Component, pageProps }) {
   return (
-    <LanguageProvider>
-      <Component {...pageProps} />
-    </LanguageProvider>
+    <>
+      <Head>
+        <link rel="icon" type="image/png" href="/favicon.png" />
+        <meta name="theme-color" content="#ffffff" />
+        <meta name="description" content="Your website description here" />
+      </Head>
+      <LanguageProvider>
+        <Component {...pageProps} />
+      </LanguageProvider>
+    </>
   );
 }
 
